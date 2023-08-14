6b92b392d825280f2567a8e9a183621c7f56ff27
Sundram Agarwal <agarwal.sundaram@gmail.com>
2023-08-14 23:34:58 +0530

    compoenets and ages and package commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 function App() {
   return (
-    <div>
-      <h1>Welcome to Chat GPT-4</h1>
-    </div>
+    <>
+      <Navbar>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Navbar>
+    </>
   );
 }
 
