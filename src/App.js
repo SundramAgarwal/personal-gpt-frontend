66a072cfebb927ac595459f28370ac1c138d7d11
Sundram Agarwal <agarwal.sundaram@gmail.com>
2023-08-15 12:51:42 +0530

    navbar.js commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import Navbar from "./components/Navbar";
 import Homepage from "./pages/Homepage";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { themeSettings } from "./theme";
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings(), []));
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </ThemeProvider>
     </>
   );
 }
